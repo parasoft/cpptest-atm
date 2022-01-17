598c605f6f2f2ab1986a7293e15652afa6aa161e
Piotr Pepek <piotr@parasoft.com>
2022-01-17 14:54:30 +0100

    Remove foo,goo,bar

--- Account.cxx
+++ Account.cxx
@@ -12,12 +12,3 @@ double Account::debit(double amount)
     myBalance -= amount;
     return (getBalance());
 }
-void foo(int accountNumber, string password)
-{
-}
-void bar(int accountNumber, string password)
-{
-}
-void goo(int accountNumber, string password)
-{
-}
